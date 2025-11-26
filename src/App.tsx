0930db6fc5a322227baa99790c5fe1971a6fe830
sharadLondon <sharad@virtualpresence.co>
2025-11-26 03:59:16 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "@/home"; 
+import Home from "@/pages/index";
 
 import SpicyWeightloss from "@/pages/blog/spicy-weightloss";
 import ThaiHerbsUk from "@/pages/blog/thai-herbs-uk";
