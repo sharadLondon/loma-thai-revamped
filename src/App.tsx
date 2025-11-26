6dba2ef8d8bf8d38327782fff5cefdaf93dfc612
sharadLondon <sharad@virtualpresence.co>
2025-11-26 03:48:01 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Home from "@/home"; 
 
-const queryClient = new QueryClient();
+import SpicyWeightloss from "@/pages/blog/spicy-weightloss";
+import ThaiHerbsUk from "@/pages/blog/thai-herbs-uk";
+import ThaiNoodles from "@/pages/blog/thai-noodles";
+import ThaiIngredients from "@/pages/blog/thai-ingredients";
+import ThaiSpices from "@/pages/blog/thai-spices";
+import ThaiCookingGuide from "@/pages/blog/thai-cooking-guide";
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Home */}
+        <Route path="/" element={<Home />} />
+
+        {/* Blog Pages */}
+        <Route path="/blog/spicy-weightloss" element={<SpicyWeightloss />} />
+        <Route path="/blog/thai-herbs-uk" element={<ThaiHerbsUk />} />
+        <Route path="/blog/thai-noodles" element={<ThaiNoodles />} />
+        <Route path="/blog/thai-ingredients" element={<ThaiIngredients />} />
+        <Route path="/blog/thai-spices" element={<ThaiSpices />} />
+        <Route path="/blog/thai-cooking-guide" element={<ThaiCookingGuide />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
