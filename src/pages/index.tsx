095939c289d84c835f4245c1a7d71c37b42b5e82
sharadLondon <sharad@virtualpresence.co>
2025-11-28 08:45:22 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 export default function Index() {
   return (
-    <div style={{ padding: "40px", fontSize: "32px" }}>
-      <h1>Homepage working</h1>
-      <p>Your routing is now fixed. Now paste your real homepage code here.</p>
-    </div>
+    <main style={{ padding: "40px" }}>
+      {/* Your real homepage content goes here */}
+    </main>
   );
 }
